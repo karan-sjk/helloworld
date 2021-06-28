a770f27c37d04abd668597290bd93447c0edf027
ADMIN <ADMIN@SJK>
2021-06-28 23:42:23 +0530

    First Commit

---
+++ Hellow.java
@@ -0,0 +1,11 @@
+package javaa_first;
+
+public class Hellow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my first program");
+
+	}
+
+}
